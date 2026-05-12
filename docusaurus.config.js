4428c177760daa1ba5778bab3db54d15477b5e0f
mdsquirosc01 <mdsquirosc01@educarex.es>
2026-05-12 11:43:06 +0200

    Cambio

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,14 +20,14 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://mdsquirosc01.github.io',
+  url: 'https://mdsquirosc01-ship-it.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/ProyectosDocusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'mdsquirosc01', // Usually your GitHub org/user name.
+  organizationName: 'mdsquirosc01-ship-it', // Usually your GitHub org/user name.
   projectName: 'ProyectosDocusaurus', // Usually your repo name.
   deploymentBranch: 'gh-pages', // Set the branch name
   trailingSlash: false,
